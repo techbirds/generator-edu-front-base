544513a379f23cd4df35eb569236bd0c2cd5b996
dong.wang <hzwangdong5@corp.netease.com>
2018-01-12 15:40:51 +0800

    Fixed copy bug

--- generators/app/index.js
+++ generators/app/index.js
@@ -10,28 +10,15 @@ module.exports = class extends Generator {
     // 本地通过link的方式依赖base是能够正常工作，但是通过npm install url依赖后
     // 终端总是提示`When copying multiple files, provide a directory as destination`
 
-    // this.fs.copy(
-    //   this.templatePath('.editorconfig'),
-    //   this.destinationPath('.editorconfig')
-    // );
-    // this.fs.copy(
-    //   this.templatePath('.eslintrc.json'),
-    //   this.destinationPath('.eslintrc.json')
-    // );
-    // this.fs.copy(
-    //   this.templatePath('.gitignore'),
-    //   this.destinationPath('.gitignore')
-    // );
-
     this.fs.copy(
-      this.templatePath('*'),
-      this.destinationRoot(), {
-        globOptions: {
-          dot: true
-        }
-      }
+      this.templatePath('.editorconfig'),
+      this.destinationPath('.editorconfig')
     );
-
+    this.fs.copy(
+      this.templatePath('.eslintrc.json'),
+      this.destinationPath('.eslintrc.json')
+    );
+    
     this.fs.copy(
         this.templatePath('gitignore'),
         this.destinationPath('.gitignore')
